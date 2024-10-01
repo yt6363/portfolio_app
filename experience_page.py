3bf8e549758db37e5fed0a5070d5afd7629e3529
yt6363 <yashnam15@gmail.com>
2024-10-01 19:31:38 -0400

    Create experience_page.py

---
+++ experience_page.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+def experience():
+    st.header("Experience")
+    st.write("Below are some of my roles and responsibilities:")
+    
+    st.subheader("Graduate Wage Associate at Pennsylvania State University")
+    st.write("""
+    - Assisted faculty in special projects and provided laboratory instruction to over 140 students.
+    - Helped with grading assignments and facilitating student engagement.
+    """)
+
+    st.subheader("Marketing Operations Intern at Incredible India Projects Pvt Ltd")
+    st.write("""
+    - Assisted in developing standards and templates for marketing operations.
+    - Managed CRM with 500+ client records, and helped in project scheduling.
+    """)
