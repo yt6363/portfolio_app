f6c209ed0061d27ab86b8b42bea06c23cecce6a4
yt6363 <yashnam15@gmail.com>
2024-10-02 01:51:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -79,8 +79,8 @@ if selected_page == "About Me":
         <a href="https://www.linkedin.com/in/yashwanth-sai-tatineni-80b4ab1b7/"_blank">
             <img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white" alt="LinkedIn">
         </a>
-        <a href="https://github.com/Yashwanth" target="_blank">
-            <img src="https://github.com/yt6363">
+        <a href="https://github.com/yt6363">
+            <img src="https://img.shields.io/badge/GitHub-181717?style=for-the-badge&logo=github&logoColor=white" alt="GitHub">
         </a>
         <a href="https://www.upwork.com/freelancers/~Yashwanth" target="_blank">
             <img src="https://img.shields.io/badge/Upwork-6fda44?style=for-the-badge&logo=upwork&logoColor=white" alt="Upwork">
