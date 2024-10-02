5a3e0bac885b22069b6a6c5e9f89ead0d4abb010
yt6363 <yashnam15@gmail.com>
2024-10-02 19:17:14 -0400

    Update experience_page.py

--- experience_page.py
+++ experience_page.py
@@ -85,7 +85,6 @@ def experience():
                 background-color: #ffffff;
                 border-radius: 10px;
                 padding: 15px;
-                box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
             ">
             """, unsafe_allow_html=True
         )
