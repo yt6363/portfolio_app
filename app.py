53bef688615427f31739bbef53cbd57c4b1eb71b
yt6363 <yashnam15@gmail.com>
2024-10-02 20:39:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ st.markdown("""
     /* Set viewport scaling for mobile devices to prevent zoom issues */
     @media only screen and (max-width: 768px) {
         html {
-            zoom: 0.1; /* Apply 40% zoom for mobile to simulate desktop view */
+            zoom: 1; /* Apply 40% zoom for mobile to simulate desktop view */
         }
     }
 
