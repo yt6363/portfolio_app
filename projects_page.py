7bd2aa61a1bc36dde3ba2e87eff68b23ba5eb366
yt6363 <yashnam15@gmail.com>
2024-10-04 17:57:55 -0400

    Update projects_page.py

--- projects_page.py
+++ projects_page.py
@@ -40,6 +40,7 @@ def projects():
         /* Link icon styling */
         .link-icons {
             display: flex;
+            align-items: center;
             gap: 15px;
             margin-top: 10px;
         }
